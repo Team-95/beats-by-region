270cd967c2fc6e50b477ec5deecfa91e3591f5f9
Matthieu Grieger <matthieugrieger@gmail.com>
2016-03-10 23:28:33 -0800

    Added simple skeleton for flask app

--- beats-by-region/beats_by_region.py
+++ beats-by-region/beats_by_region.py
@@ -10,3 +10,15 @@
     :copyright: (c) 2016 Team 95.
     :license: MIT, see LICENSE for more details.
 """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    app.run()
