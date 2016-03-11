eed509429d1011642059fea7c305d068ae37c9ea
Matthieu Grieger <matthieugrieger@gmail.com>
2016-03-10 23:12:51 -0800

    Add beats-by-region/main.py

---
+++ beats-by-region/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Beats By Region
+    ---------------
+
+    A website that generates music playlists based on selected
+    geographical regions.
+
+    :copyright: (c) 2016 Team 95.
+    :license: MIT, see LICENSE for more details.
+"""
