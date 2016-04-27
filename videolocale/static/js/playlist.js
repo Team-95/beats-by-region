077f9c2f993a5b55d899fa465adcdf1e8a618544
MarshallPelissier <pelissierm@spu.edu>
2016-04-27 15:59:24 -0700

    Added class for open videos for easier access

--- videolocale/static/js/playlist.js
+++ videolocale/static/js/playlist.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
   //expand individual song
   $('.playlist-video-header').click(function(){
     var $next = $(this).next('.playlist-video-outside');
-    $('.playlist-video-outside').not($next).hide();
-    $next.toggle();
+    $('.playlist-video-outside').not($next).removeClass('.playlist-video-outside-open');
+    $($next).toggleClass('playlist-video-outside-open');
+    //if (($next).hasClass('playlist-video-outside-open')){
+    //  var link = $(this).attr('id');
+    //  ($next).html(link);
+    //}
+
     //var video = $next.children(":first").children(":first").children(":first");
     //var video = $next.find('iframe');
     //var video = $('#video');
