eb18309eb8bb83b44650bcdeed07487e8216e6b6
MarshallPelissier <pelissierm@spu.edu>
2016-04-27 17:06:30 -0700

    Videos now load and stop when toggled

--- videolocale/static/js/playlist.js
+++ videolocale/static/js/playlist.js
@@ -2,15 +2,20 @@ $(document).ready(function() {
   //expand individual song
   $('.playlist-video-header').click(function(){
     var $next = $(this).next('.playlist-video-outside');
-    $('.playlist-video-outside').not($next).removeClass('.playlist-video-outside-open');
+    $('.playlist-video-outside').not($next).removeClass('playlist-video-outside-open');
     $($next).toggleClass('playlist-video-outside-open');
-    //if (($next).hasClass('playlist-video-outside-open')){
-    //  var link = $(this).attr('id');
-    //  ($next).html(link);
-    //}
 
-    //var video = $next.children(":first").children(":first").children(":first");
-    //var video = $next.find('iframe');
+    var video = $next.find('iframe');
+    //load video (a little slow)
+    if (($next).hasClass('playlist-video-outside-open')){
+      var link = $(this).attr('id');
+      $(video).attr('src','http://www.youtube.com/embed/'+link);
+    }
+    //delete video
+    else{
+      $(video).attr("src","");
+    }
+
     //var video = $('#video');
     //var vidURL = $(video).attr("src");
     //$("#video").attr("src","");
