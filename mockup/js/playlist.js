36335e297a819b435e7d5fd2c269d22694b7f0c6
unknown <pelissierm@OMH253-10.spu.local>
2016-04-29 18:09:03 -0700

    Started on dynamically moving the video description when page size changes in MOCKUP

--- mockup/js/playlist.js
+++ mockup/js/playlist.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   //expand individual song
   $('.playlist-video-header').click(function(){
     var $next = $(this).next('.playlist-video-outside');
+    //var $nextHeader = $(this).closest().next().find('.playlist-video-header');
+    //var $nextHeader = $(this).closest('.playlist-video-header');
+    var $nextHeader = $($next).next('.playlist-video-header');
     $('.playlist-video-outside').not($next).hide();
     $next.toggle();
     //var video = $next.children(":first").children(":first").children(":first");
@@ -10,5 +13,16 @@ $(document).ready(function() {
     //var vidURL = $(video).attr("src");
     //$("#video").attr("src","");
     //$("#video").attr("src",vidURL);
+    console.log($nextHeader);
+    console.log($next);
+    if ($nextHeader.length){
+      console.log ('something');
+
+    }
+    else{
+      console.log('nothing');
+      //check if open then
+      $($next).toggleClass('playlist-video-header-bottom');
+    }
   });
 });
